e416db54d67a2043d93c64095a037c006d8711f7
Priscilla-Rojas <priscilla.k.rojas@hotmail.com>
2021-08-12 11:29:38 -0300

    agregue mas variantes para que el no lance errores de nan y funcioneen todos los botones

--- app.js
+++ app.js
@@ -1,10 +1,11 @@
 let buy = document.getElementsByClassName('buy')[0];
 let numberPerson = document.querySelector('.number-person');
-let porcentajePropina = document.querySelector('.porcentje-button');  
+let porcentajePropina = document.querySelectorAll('.porcentje-button');  
 let pay = document.getElementsByClassName('pay')[0];
 let personPay = document.querySelector('.person-pay');
 let reset = document.querySelector('.reset');
 
+console.log(porcentajePropina)
 function calculoPropina(total, porcentaje){
     let propinaPorcentaje = (total * porcentaje) / 100;
     return propinaPorcentaje;
@@ -13,18 +14,50 @@ function totalPersona(total, personas){
     let  calculo = total / personas;
     return calculo
 }
-porcentajePropina.addEventListener('click', ()=>{
-    let porcentaje = porcentajePropina.value[0];
-    pay.placeholder = calculoPropina(buy.value, porcentaje);
-    reset.style.backgroundColor ="#26c0ab";
-})
+for (let i = 0; i < porcentajePropina.length; i++) {
+    porcentajePropina[i].addEventListener('click', ()=>{
+        let porcentaje;
+        if(numberPerson == ''){
+            if(i == 0){
+                porcentaje = porcentajePropina[i].value[0];
+            }else{
+                porcentaje = porcentajePropina[i].value[0];
+                porcentaje += porcentajePropina[i].value[1]
+            }
+            pay.placeholder = calculoPropina(buy.value, porcentaje);
+            reset.style.backgroundColor ="#26c0ab";
+        }else{
+            if(i == 0){
+                        porcentaje = porcentajePropina[i].value[0];
+                    }else{
+                        porcentaje = porcentajePropina[i].value[0];
+                        porcentaje += porcentajePropina[i].value[1]
+                    }
+                    pay.placeholder = calculoPropina(buy.value, porcentaje);
+                    reset.style.backgroundColor ="#26c0ab";
+                    // mostrar en display
+                    let numberPersons = numberPerson.value;
+                    personPay.placeholder = totalPersona(pay.placeholder, numberPersons);
+                
+                    if(personPay.placeholder == Infinity) {
+                        personPay.placeholder = totalPersona(pay.placeholder, 1);
+                    }else if(isNaN(personPay.placeholder)){
+                        pay.placeholder = buy.value;
+                        personPay.placeholder = totalPersona(pay.placeholder, numberPersons);
+                    }
+        }
+    })
+}
 
 numberPerson.addEventListener('keyup', ()=>{
     let numberPersons = numberPerson.value;
     personPay.placeholder = totalPersona(pay.placeholder, numberPersons);
 
-    if(personPay.placeholder == Infinity){
+    if(personPay.placeholder == Infinity) {
         personPay.placeholder = totalPersona(pay.placeholder, 1);
+    }else if(isNaN(personPay.placeholder)){
+        pay.placeholder = buy.value;
+        personPay.placeholder = totalPersona(pay.placeholder, numberPersons);
     }
 
 })
